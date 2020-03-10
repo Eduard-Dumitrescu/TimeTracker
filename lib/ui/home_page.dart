da05e72583050aae1760b47f2f07923f3029251c
Eduard-Dumitrescu <eduard.dumitrescu94@gmail.com>
2020-03-10 23:35:23 +0200

    make homepage stateless

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   final String title;
 
   const HomePage({Key key, this.title}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body:
           Container(), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    ;
   }
 }
